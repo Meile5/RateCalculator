b89a3f109d60962ef38c8fc78d7775a30a9485db
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-04-29 10:27:04 +0200

    added the map to the view

--- src/easv/ui/pages/homePage/HomePageController.java
+++ src/easv/ui/pages/homePage/HomePageController.java
@@ -1,5 +1,6 @@
 package easv.ui.pages.homePage;
 import easv.ui.components.homePage.empty.SideNavigationNewAproach;
+import easv.ui.components.homePage.map.WorldMap;
 import easv.ui.components.homePage.scrollPane.ScrollPaneContoller;
 import easv.ui.components.homePage.sideNavigation.SideNavigation;
 import javafx.application.Platform;
@@ -20,7 +21,9 @@ import java.util.ResourceBundle;
 public class HomePageController implements Initializable {
     private Parent root;
     @FXML
-    private StackPane menu;
+    private StackPane menu,firstLayout;
+    @FXML
+    private VBox pageContainer;
     public HomePageController() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("HomePage.fxml"));
         loader.setController(this);
@@ -40,11 +43,16 @@ public class HomePageController implements Initializable {
         ScrollPaneContoller sde = new ScrollPaneContoller();
         Platform.runLater(()->{ initializeSideMenu(menu,sde.getRoot());});
         Platform.runLater(()->{sde.getRoot().getWidth();});
+        initializeWorldMap();
     }
 
     private void initializeSideMenu(StackPane stackPane, ScrollPane hBox){
         StackPane.setAlignment(hBox,Pos.CENTER_LEFT);
         stackPane.getChildren().add(hBox);
-
     }
+    private void initializeWorldMap(){
+        WorldMap worldMap = new WorldMap(firstLayout);
+        this.pageContainer.getChildren().add(worldMap.getRoot());
+    }
+
 }
