fcd3a0fafb7d0f8fdbafd9846e4981e997551a9c
Meile5 <meile.kapisaite5@gmail.com>
2024-05-06 18:27:50 +0200

    styling changes

--- src/easv/ui/pages/employeesPage/employeeMainPage/EmployeeMainPageController.java
+++ src/easv/ui/pages/employeesPage/employeeMainPage/EmployeeMainPageController.java
@@ -23,6 +23,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.SVGPath;
 import javafx.util.Duration;
 
 import java.awt.event.ActionEvent;
@@ -57,9 +58,8 @@ public class EmployeeMainPageController implements Initializable , DisplayEmploy
     @FXML
     private Button goBackButton;
     private Service<Void> loadEmployeesFromDB;
-    private final Image revertImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/easv/resources/images/revert.png")));
-    private final Image searchImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/easv/resources/images/search (1).png")));
-
+    @FXML
+    private SVGPath svgPath;
 
 
     private EmployeeInfoController selectedToEdit;
@@ -88,7 +88,7 @@ public class EmployeeMainPageController implements Initializable , DisplayEmploy
 
             model = ModelFactory.createModel(ModelFactory.ModelType.NORMAL_MODEL);
             progressBar.setVisible(true);
-            showSearchImage();
+            loadSearchSVG();
             initializeEmployeeLoadingService();
 
             createPopUpWindow();
@@ -140,17 +140,26 @@ public class EmployeeMainPageController implements Initializable , DisplayEmploy
         loadEmployeesFromDB.restart();
     }
 
-    private void showRevertImage(){
-        goBackButton.setGraphic(new ImageView(revertImage));
-    }
-    private void showSearchImage(){
-        goBackButton.setGraphic(new ImageView(searchImage));
+    // Method to load the first SVG path
+    private void loadSearchSVG() {
+        String defaultSVGPath = "M269.46,1163.45 C263.17,1163.45 258.071,1158.44 258.071,1152.25 C258.071,1146.06 263.17,1141.04 269.46,1141.04 C275.75,1141.04 280.85,1146.06 280.85,1152.25 C280.85,1158.44 275.75,1163.45 269.46,1163.45 L269.46,1163.45 Z M287.688,1169.25 L279.429,1161.12 C281.591,1158.77 282.92,1155.67 282.92,1152.25 C282.92,1144.93 276.894,1139 269.46,1139 C262.026,1139 256,1144.93 256,1152.25 C256,1159.56 262.026,1165.49 269.46,1165.49 C272.672,1165.49 275.618,1164.38 277.932,1162.53 L286.224,1170.69 C286.629,1171.09 287.284,1171.09 287.688,1170.69 C288.093,1170.3 288.093,1169.65 287.688,1169.25 L287.688,1169.25 Z";
+        svgPath.getStyleClass().clear();
+        svgPath.getStyleClass().add("svg-search");
+        svgPath.setContent(defaultSVGPath);
     }
 
+    // Method to load the second SVG path
+    private void loadRevertSVG() {
+        String alternativeSVGPath = "M 15 3 L 10 7 L 15 11 L 15 8 C 18.877838 8 22 11.12216 22 15 C 22 18.87784 18.877838 22 15 22 C 11.122162 22 8 18.87784 8 15 C 8 13.485854 8.4798822 12.090114 9.2910156 10.947266 L 7.8730469 9.5292969 C 6.7042423 11.047902 6 12.942076 6 15 C 6 19.95872 10.041282 24 15 24 C 19.958718 24 24 19.95872 24 15 C 24 10.04128 19.958718 6 15 6 L 15 3 z ";
+        svgPath.getStyleClass().clear();
+        svgPath.getStyleClass().add("svg-revert");
+        svgPath.setContent(alternativeSVGPath);
+    }
     @FXML
     private void goBack() throws RateException {
         model.performEmployeeSearchUndoOperation();
-        Platform.runLater(this::showSearchImage);
+        searchField.clear();
+        Platform.runLater(this::loadSearchSVG);
 
     }
 
@@ -184,7 +193,8 @@ public class EmployeeMainPageController implements Initializable , DisplayEmploy
 
                     }
                 } else {
-                    showRevertImage();
+                    loadRevertSVG();
+                    searchField.clear();
                     popupWindow.hide();
                 }
 
@@ -210,7 +220,7 @@ public class EmployeeMainPageController implements Initializable , DisplayEmploy
                         searchResponseHolder.getSelectionModel().clearSelection();
                     }
                 });
-                showRevertImage();
+                loadRevertSVG();
                 popupWindow.hide();
             }
         });
