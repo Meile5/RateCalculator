5f7afa2d8bdd69cc79a613bbd1acc42740c375c8
Nelson  Veríssimo <nelson.sverissimo@gmail.com>
2024-05-13 19:19:24 +0200

    fixed changes on add employee

--- src/easv/dal/IEmployeeDAO.java
+++ src/easv/dal/IEmployeeDAO.java
@@ -7,16 +7,18 @@ import javafx.collections.ObservableMap;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public interface IEmployeeDAO {
     LinkedHashMap<Integer, Employee> returnEmployees() throws RateException;
 
-    Integer addEmployee(Employee employee, boolean newCountry, boolean newTeam, Configuration configuration) throws RateException;
+    Integer addEmployee(Employee employee, Configuration configuration, List<Team> teams) throws RateException;
     Boolean deleteEmployee(Employee employee) throws RateException;
     void addNewCountryOrTeam(Employee employee, boolean newCountry, boolean newTeam, Connection conn) throws RateException, SQLException;
     Integer addCountry(Country country, Connection conn) throws RateException, SQLException;
     Integer addTeam(Team team, Connection conn) throws RateException, SQLException;
+    void addEmployeeToTeam(int employeeID, List<Team> teams, Connection conn) throws RateException, SQLException;
     Integer addConfiguration(Configuration configuration, Connection conn) throws RateException, SQLException;
     void addEmployeeConfiguration(int employeeID, int configurationID, Connection conn) throws RateException, SQLException;
 
